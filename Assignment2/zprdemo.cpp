2980b8126f7d5fd48fe9bb3b003008c14acedd41
Edward Ayala <edwardayy123@gmail.com>
2020-01-29 00:04:54 -0600

    Window Title update

--- Assignment2/zprdemo.cpp
+++ Assignment2/zprdemo.cpp
@@ -278,7 +278,7 @@ int main(int argc, char* argv[])
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize(400, 400);
-    glutCreateWindow("GLT Mouse Zoom-Pan-Rotate");
+    glutCreateWindow("GLT Mouse Zoom-Pan-Rotate - Edward Ayala");
 
     /* Configure GLUT callback functions */
 
